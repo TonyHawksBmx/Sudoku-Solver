79973c9cde6902ad5900983c5078d52ba87c92fa
TonyHawksBmx <dev729412@gmail.com>
2025-02-01 15:52:11 +0530

    v1.3 : Updated Display using ncurses

--- Sudoku.c
+++ Sudoku.c
@@ -1,64 +1,142 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<ncurses.h>
 #define N 9
 
-void enterSudoku(int (*A)[N]);
-void printSudoku(int (*A)[N]);
-int solveSudoku(int (*A)[N], int row , int col);
+void enterSudoku(int (*A)[N], WINDOW *W);
+void printSudoku(int (*A)[N], WINDOW *W);
+void resetSudoku(int (*A)[N], WINDOW *W);
+int solveSudoku(int (*A)[N], int row , int col, WINDOW *W);
 int checkLegal(int (*A)[N], int row, int col, int num);
 
 int main(){
-	int exit_status = system("gnome-terminal");
 	int A[N][N]={
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,2,0,0,5,1,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,3,0,0,8,0,0,0},
-		{0,0,0,7,4,3,0,0,0},
-		{0,0,7,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0,0,0}};
-	//enterSudoku(A);
-	printSudoku(A);
-	printf("\n\n");
-	if (solveSudoku(A,0,0) == 1)
-		printSudoku(A);
-	else
-		printf("\nNo solution.");
+		{0,0,0,0,1,0,0,0,0},
+		{0,0,0,0,2,0,0,0,0},
+		{0,0,0,0,3,0,0,0,0},
+		{0,0,0,0,4,0,0,0,0},
+		{0,0,0,0,5,0,0,0,0},
+		{0,0,0,0,6,0,0,0,0},
+		{0,0,0,0,7,0,0,0,0},
+		{0,0,0,0,8,0,0,0,0},
+		{0,0,0,0,9,0,0,0,0}};
+		
+	WINDOW *w , *w1;
+	
+	char list[5][16] = {
+		"Enter Sudoku ->",
+		"Print Sudoku ->",
+		"Solve Sudoku ->",
+		"Reset Sudoku.",
+		"Exit!"};
+	
+	char item[16];
+	
+	int ch, i=0, j=0, width = 16;
+	
+	initscr();
+	
+	w = newwin( 8, 20, 3, 1 );
+	w1 = newwin (11, 28, 3, 30);
+	box( w, '|', '-' );
+	box( w1, '|', '-');
+	mvwprintw(stdscr,2,1,"%s","MENU");
+	mvwprintw(stdscr,2,30,"%s","SUDOKU");
+	for( i=0; i<5; i++ ){
+		if( i == 0 ) 
+			wattron( w, A_STANDOUT );
+		else
+			wattroff( w, A_STANDOUT );
+		sprintf(item, "%-15s",  list[i]);
+		mvwprintw( w, i+1, 2, "%s", item );
+	}
+	wrefresh( w );
+	
+	printSudoku(A,w1);
+
+	noecho();
+	keypad( w, TRUE );
+	curs_set( 0 );
+	
+	while(( ch = wgetch(w)) != 'q'){
+		sprintf(item, "%-15s",  list[i]); 			
+		mvwprintw( w, i+1, 2, "%s", item ); 
+		switch( ch ) {
+			case KEY_UP:
+				i--;
+				i = ( i<0 ) ? 4 : i;
+				break;
+			case KEY_DOWN:
+				i++;
+				i = ( i>4 ) ? 0 : i;
+				break;
+			case 10:
+				switch(i){
+					case 0:
+						enterSudoku(A,w1);
+						break;
+					case 1:
+						printSudoku(A,w1);
+						break;
+					case 2:
+						solveSudoku(A,0,0,w1);
+						break;
+					case 3:
+						resetSudoku(A,w1);
+						break;
+					case 4:
+						delwin(w);
+						delwin(w1);
+						endwin();
+						return 0;
+				}
+		}
+		wattron( w, A_STANDOUT );
+		sprintf(item, "%-15s",  list[i]);
+		mvwprintw( w, i+1, 2, "%s", item);
+		wattroff( w, A_STANDOUT );
+	}
+	delwin( w );
+	delwin( w1 );
+	endwin();
 	return 0;
 }
 
-	
-void enterSudoku (int (*A)[N]){
+void enterSudoku (int (*A)[N],WINDOW *w){
 	for(int i=0;i<N;i++){
 		for (int j=0;j<N;j++){
 			char c = 'A';
 			int Num = 10;
-			while (Num>N || N<0){
-				c=getchar();
+			wattron(w, A_STANDOUT);
+			mvwprintw(w, i+1, 3*j+1, "%d", A[i][j]);
+			while (Num>N || Num<0){
+				c=wgetch(w);
 				Num=c-'0';
 			}
-			A[i][j]=N;
-			printf ("\t");
+			A[i][j]=Num;
+			mvwprintw(w, i+1, 3*j+1, "%d", A[i][j]);
+			wattroff(w, A_STANDOUT);
+			mvwprintw(w, i+1, 3*j+1, "%d", A[i][j]);
 		}
 	}
+	wrefresh(w);
 }
 
-void printSudoku (int (*A)[N]){
-	for (int i =0; i<N ; i++){
-		for (int j =0; j<N; j++){
-			if (A[i][j] == 0)
-				printf(" \t");
-			else
-				printf("%d\t",A[i][j]);
-		}
-		printf("\n\n");
-	}
+void printSudoku (int (*A)[N], WINDOW *w){
+	for (int i =0; i<N ; i++)
+		for (int j =0; j<N; j++)
+			mvwprintw( w, i+1, 3*j+1, "%d", A[i][j]);
+	wrefresh(w);
 }
 
-int solveSudoku (int (*A)[N], int row, int col){
+void resetSudoku(int (*A)[N], WINDOW *w){
+	for (int i =0; i<N ; i++)
+		for (int j =0; j<N; j++)
+			A[i][j]=0;
+	printSudoku(A,w);		
+}
+
+int solveSudoku (int (*A)[N], int row, int col, WINDOW *w){
 	if (row == N - 1 && col == N)
 		return 1;
 	if (col == N){
@@ -66,11 +144,11 @@ int solveSudoku (int (*A)[N], int row, int col){
 		col = 0;
 	}
 	if (A[row][col] > 0)
-		return solveSudoku(A, row, col+1);
+		return solveSudoku(A, row, col+1,w);
 	for (int num =1; num <=N; num ++){
 		if (checkLegal(A,row,col,num)){
 			A[row][col]=num;
-			if (solveSudoku(A,row,col+1) == 1)
+			if (solveSudoku(A,row,col+1,w) == 1)
 				return 1;
 		}
 		A[row][col] = 0;
